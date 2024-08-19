e097072508a34c7714150b85bd5fd9d869539800
Sarahmarie Hollenstein <sholle7@gmail.com>
2024-08-19 14:24:59 -0500

    ✨ Add Pydantic schemas and update API endpoints

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,6 @@
-from fastapi import Depends, HTTPException
+from fastapi import FastAPI, Depends, HTTPException
 from sqlalchemy.orm import Session
-from . import crud, models, database
+from . import crud, models, database, schemas
 
 models.Base.metadata.create_all(bind=database.engine)
 
@@ -13,30 +13,23 @@ def get_db():
     finally:
         db.close()
 
-@app.post("/recipes/")
-def create_recipe(recipe: models.Recipe, db: Session = Depends(get_db)):
-    return crud.create_recipe(db=db, recipe=recipe)
+@app.post("/recipes/", response_model=schemas.Recipe)
+def create_recipe(recipe: schemas.RecipeCreate, db: Session = Depends(get_db)):
+    return crud.create_recipe(db=db, recipe=models.Recipe(**recipe.dict()))
 
-@app.get("/recipes/{recipe_id}")
+@app.get("/recipes/{recipe_id}", response_model=schemas.Recipe)
 def read_recipe(recipe_id: int, db: Session = Depends(get_db)):
     db_recipe = crud.get_recipe(db=db, recipe_id=recipe_id)
     if db_recipe is None:
         raise HTTPException(status_code=404, detail="Recipe not found")
     return db_recipe
 
-@app.put("/recipes/{recipe_id}")
-def update_recipe(recipe_id: int, updated_recipe: models.Recipe, db: Session = Depends(get_db)):
+@app.put("/recipes/{recipe_id}", response_model=schemas.Recipe)
+def update_recipe(recipe_id: int, updated_recipe: schemas.RecipeCreate, db: Session = Depends(get_db)):
     db_recipe = crud.get_recipe(db=db, recipe_id=recipe_id)
     if db_recipe is None:
         raise HTTPException(status_code=404, detail="Recipe not found")
-    db_recipe.name = updated_recipe.name
-    db_recipe.prep_time = updated_recipe.prep_time
-    db_recipe.cook_time = updated_recipe.cook_time
-    db_recipe.servings = updated_recipe.servings
-    db_recipe.instructions = updated_recipe.instructions
-    db.commit()
-    db.refresh(db_recipe)
-    return db_recipe
+    return crud.update_recipe(db=db, recipe_id=recipe_id, updated_recipe=models.Recipe(**updated_recipe.dict()))
 
 @app.delete("/recipes/{recipe_id}")
 def delete_recipe(recipe_id: int, db: Session = Depends(get_db)):
