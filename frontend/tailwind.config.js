005ce53ebf231e07b3461f455b6c9f2937b20533
Sarahmarie Hollenstein <sholle7@gmail.com>
2024-08-19 14:56:41 -0500

    💄 Install and configure Tailwind CSS with custom colors

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,18 +1,20 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      colors: {
+        primary: {
+          light: '#F3BFC4', // Light Pink
+          DEFAULT: '#F29BAA', // Main Pink from logo
+          dark: '#D17384', // Darker Pink
+        },
+        secondary: '#000000', // Black
+        background: '#FFFFFF', // White/Light Pink
       },
     },
   },
   plugins: [],
-};
+}
