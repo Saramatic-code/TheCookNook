467306fbc09094f39ffcec84f42c5c7b221d9764
Sarahmarie Hollenstein <sholle7@gmail.com>
2024-08-19 14:09:10 -0500

    🚀 Add API endpoints for CRUD operations

--- backend/main.py
+++ backend/main.py
@@ -1,11 +1,23 @@
-from fastapi import FastAPI
-from . import models
-from database import engine
+from fastapi import Depends, HTTPException
+from sqlalchemy.orm import Session
+from . import crud, models, database
 
-models.Base.metadata.create_all(bind=engine)
+models.Base.metadata.create_all(bind=database.engine)
 
-app = FastAPI()
+def get_db():
+    db = database.SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
 
-@app.get("/")
-def read_root():
-    return {"message": "Welcome to the Healthy Recipe App!"}
+@app.post("/recipes/")
+def create_recipe(recipe: models.Recipe, db: Session = Depends(get_db)):
+    return crud.create_recipe(db=db, recipe=recipe)
+
+@app.get("/recipes/{recipe_id}")
+def read_recipe(recipe_id: int, db: Session = Depends(get_db)):
+    db_recipe = crud.get_recipe(db=db, recipe_id=recipe_id)
+    if db_recipe is None:
+        raise HTTPException(status_code=404, detail="Recipe not found")
+    return db_recipe
