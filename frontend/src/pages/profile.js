2d10788f3c74198108bbcc5020ce4ab7066ed96d
Sarahmarie Hollenstein <sholle7@gmail.com>
2024-08-23 15:48:28 -0500

    🔧 Updated profile page and added pages for sidebar menu to work

--- frontend/src/pages/profile.js
+++ frontend/src/pages/profile.js
@@ -46,24 +46,40 @@ export default function Profile() {
     return (
         <div className="wrapper flex">
             <Navbar />
-            <div className="flex flex-row w-full p-4">
+            <div className="flex flex-row w-full p-4 justify-center">
                 {/* Sidebar Menu */}
-                <div className="sidebar p-4 bg-gray-100 w-1/4 mr-4">
+                <div className="sidebar p-4 bg-gray-100 w-1/6 mr-4">
                     <h2 className="font-semibold text-lg mb-4">Profile Settings</h2>
                     <ul>
-                        <li className="mb-2"><a href="#personal-info">Personal Info</a></li>
-                        <li className="mb-2"><a href="#activity">User Activity</a></li>
-                        <li className="mb-2"><a href="#account-settings">Account Settings</a></li>
-                        <li className="mb-2"><a href="#social-features">Social Features</a></li>
-                        <li className="mb-2"><a href="#personalization">Personalization</a></li>
-                        <li className="mb-2"><a href="#user-contributions">User Contributions</a></li>
-                        <li className="mb-2"><a href="#stats">Statistics & Analytics</a></li>
-                        <li className="mb-2"><a href="#logout">Logout</a></li>
+                        <li className="mb-2 cursor-pointer">
+                            <a href="/profile#personal-info">Personal Info</a>
+                        </li>
+                        <li className="mb-2 cursor-pointer">
+                            <a href="/favorites">Favorites</a>
+                        </li>
+                        <li className="mb-2 cursor-pointer">
+                            <a href="/recentlyViewed">Recently Viewed</a>
+                        </li>
+                        <li className="mb-2 cursor-pointer">
+                            <a href="/uploadedRecipes">Uploaded Recipes</a>
+                        </li>
+                        <li className="mb-2 cursor-pointer">
+                            <a href="/savedRecipes">Saved Recipes</a>
+                        </li>
+                        <li className="mb-2 cursor-pointer">
+                            <a href="/cookingHistory">Cooking History</a>
+                        </li>
+                        <li className="mb-2 cursor-pointer">
+                            <a href="/accountSettings">Account Settings</a>
+                        </li>
+                        <li className="mb-2 cursor-pointer">
+                            <a href="/logout">Logout</a>
+                        </li>
                     </ul>
                 </div>
 
                 {/* Main Content Area */}
-                <div className="main-content flex-1 p-4 text-[#696969]">
+                <div className="main-content max-w-xl mx-auto p-4 text-[#696969]">
                     <div id="personal-info" className="mb-8">
                         <h1 className="text-3xl font-bold mb-4">Personal Info</h1>
                         <img
@@ -71,12 +87,7 @@ export default function Profile() {
                             alt="Profile Pic"
                             className="mx-auto rounded-full h-24 w-24 object-cover mb-4"
                         />
-                        <button className="inline-block mb-4 text-[#696969] cursor-pointer hover:underline">
-                            Change Profile Picture
-                        </button>
-                        <button onClick={removeProfileImage} className="mb-4 p-2 bg-red-500 text-white rounded hover:bg-red-600">
-                            Remove Profile Picture
-                        </button>
+
                         <input
                             type="text"
                             value={username}
@@ -103,15 +114,24 @@ export default function Profile() {
                         />
                     </div>
 
-                    {/* Add the rest of the sections (activity, account settings, etc.) here */}
+                    <div className="flex justify-center space-x-4 mt-8">
+                        <button className="p-2 bg-[#F29BAA] text-white rounded hover:bg-[#D17384]">
+                            Change Profile Picture
+                            <input type="file" onChange={handleImageUpload} className="hidden" />
+                        </button>
+                        <button onClick={removeProfileImage} className="p-2 bg-red-500 text-white rounded hover:bg-red-600">
+                            Remove Profile Picture
+                        </button>
+                    </div>
 
-                    <div className="flex justify-end space-x-4 mt-8">
-                        <button onClick={handleSave} className="p-2 bg-blue-500 text-white rounded hover:bg-blue-600">
+                    <div className="flex flex-col items-center space-y-4 mt-8">
+                        <button onClick={handleSave} className="p-2 bg-[#F29BAA] text-white rounded hover:bg-[#D17384]">
                             Save Profile Information
                         </button>
                         <button onClick={sendTestSMS} className="p-2 bg-green-500 text-white rounded hover:bg-green-600">
                             Send Test SMS
                         </button>
+
                     </div>
                 </div>
             </div>
